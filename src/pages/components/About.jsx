ac8580dee2ddd6fc6d36208c27227e7518d1b262
rizkaputris <rizkap214@gmail.com>
2023-06-24 17:04:49 +0700

    first commit

---
+++ src/pages/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+
+export default function About() {
+  return (
+    <div>
+        
+        <Header/>
+        About Page
+
+    </div>
+  )
+}
